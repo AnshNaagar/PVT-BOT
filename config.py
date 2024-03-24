becf1d0d82ec0f3df72e7c9c3a10590a4fdb2f62
AnshNaagar <158273761+AnshNaagar@users.noreply.github.com>
2024-03-24 23:39:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TeamAnsh",
+    "https://github.com/AnshNaagar/PVT-BOT",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
